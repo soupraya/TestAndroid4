d4a49dff11058e65e6a97d502da42a17174e1917
Velan <soupraya@gmail.com>
2015-04-04 16:11:42 +0200

    Navigation drawer fixed

--- TestAndroid4/src/com/velan/testandroid4/TodoDetailsInputFragment.java
+++ TestAndroid4/src/com/velan/testandroid4/TodoDetailsInputFragment.java
@@ -10,8 +10,10 @@ import com.velan.testandroid4.todoendpoint.model.Todo;
 
 import android.app.Fragment;
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +33,7 @@ public class TodoDetailsInputFragment extends Fragment {
 	private EditText task_bt;
 	private TextView task_ct;
 	private Button save_todo;
+	Context thiscontext;
 	
 	/**
 	   * Initializes the activity content, binds relevant widgets and starts asynchronously retrieving
@@ -40,7 +43,19 @@ public class TodoDetailsInputFragment extends Fragment {
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
 		      Bundle savedInstanceState) {
 	        //super.onActivityCreated(savedInstanceState);
-	    
+	        if (container == null) {
+	            // We have different layouts, and in one of them this
+	            // fragment's containing frame doesn't exist.  The fragment
+	            // may still be created from its saved state, but there is
+	            // no reason to try to create its view hierarchy because it
+	            // won't be displayed.  Note this is not needed -- we could
+	            // just run the code below, where we would create and return
+	            // the view hierarchy; it would just never be used.
+	            return null;
+	        }
+		  thiscontext = container.getContext();
+		  
+		  
 	    //View TodoDetailsInputFragmentView = getActivity().findViewById(R.id.todo_details_input_fragment);
 		  View view = inflater.inflate(R.layout.todo_detail_input_fragment,
 			        container, false);
@@ -53,13 +68,24 @@ public class TodoDetailsInputFragment extends Fragment {
 	    task_bt = (EditText) view.findViewById(R.id.task_bt);
 	    task_ct = (TextView) view.findViewById(R.id.task_ct);
 	    save_todo = (Button) view.findViewById(R.id.save_todo);
-
+	    
+	    if(currenttodo != null)
+	    {
 	    todo_desc.setText(currenttodo.getShortDescription());
 	    todo_long_desc.setText(currenttodo.getLongDescription());
-	    
+	    todo_a.setText(currenttodo.getA().toString());
+	    todo_b.setText(currenttodo.getA().toString());
+	    //task_at.setText(currenttask.getAt().toString());
+	    //task_bt.setText(currenttask.getBt().toString());
+	    }
+	    else
+	    {
+	    	currenttodo = new Todo();
+	    }
 	    save_todo.setOnClickListener(new OnClickListener(){
 	    		  @Override
 	    		  public void onClick(View v) {
+	    			  Log.i("TodoDetailsInputFragment", "update todo click");
 	    			  currenttodo.setShortDescription(todo_desc.getText().toString());
 	    			  currenttodo.setLongDescription(todo_long_desc.getText().toString());
 	    			  currenttodo.setA(Double.parseDouble(todo_a.getText().toString()));
@@ -83,7 +109,7 @@ public class TodoDetailsInputFragment extends Fragment {
 
 		@Override
 		protected Todo doInBackground(Void... arg0) {
-			
+			Log.i("TodoDetailsInputFragment", "TodoAsyncUpdate");
 		    Todoendpoint.Builder endpointBuilder = new Todoendpoint.Builder(
 		    AndroidHttp.newCompatibleTransport(), new JacksonFactory(), null)
 		    .setApplicationName("testandroid4");
@@ -92,21 +118,35 @@ public class TodoDetailsInputFragment extends Fragment {
 		    Todoendpoint endpoint = endpointBuilder.build();
 
 		    Todo result;
-		      try {
-			        result = endpoint.updateTodo(currenttodo).execute();
-			      } catch (IOException e) {
-			        // TODO Auto-generated catch block
-			        e.printStackTrace();
-			        result = null;
-			      }
-			      return result;
+
+			      try {
+			    	  
+					    if(currenttodo.getId() == null)
+					    {
+					    	currenttodo.setId(System.currentTimeMillis());
+					    	Log.i("TodoDetailsInputFragment", "Insert Todo");
+					    	result = endpoint.insertTodo(currenttodo).execute();
+					    }
+					    else {
+					    	Log.i("TodoDetailsInputFragment", "Update Todo");
+					    	result = endpoint.updateTodo(currenttodo).execute();
+					    }
+					    
+				        
+				      } catch (IOException e) {
+				        // TODO Auto-generated catch block
+				        e.printStackTrace();
+				        result = null;
+				      }
+				      return result;		    	
 
 		}
-	    /*@Override
+		
+	    @Override
 	    protected void onPostExecute(Todo result) {
 	    	String displayString = "Todo saved!";
-	    	Toast msg = Toast.makeText(getBaseContext(), displayString,Toast.LENGTH_LONG);
+	    	Toast msg = Toast.makeText(getActivity().getBaseContext(), displayString,Toast.LENGTH_LONG);
 	    	msg.show();
-			}*/
+			}
 		}
 }
